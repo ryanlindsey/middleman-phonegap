6d9ff7cafa708f1ed1021cc6b60d5612e86b0fc0
Ryan Lindsey <ryan@pixelsonly.com>
2013-02-06 10:44:28 -0800

    bug fix #1: resolved build directory conflict

--- phonegap.rb
+++ phonegap.rb
@@ -5,12 +5,13 @@
 module Phonegap
   class << self
     def registered(app)
+      app.set :build_dir, 'mm_build'
       app.set :css_dir, 'stylesheets'
       app.set :js_dir, 'javascripts'
       app.set :images_dir, 'images'
 
       app.after_build do |builder|
-        builder.run('rm -R www && mv build www && ./cordova/build && ./cordova/emulate')
+        builder.run('rm -R www && mv mm_build www && ./cordova/build && ./cordova/emulate')
       end
     end
     alias :included :registered
